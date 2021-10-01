ca3721f751a3ce5e13fb82b36382242801a8b4e7
AriosJentu <darthpoezd@gmail.com>
2021-10-02 01:01:38 +1000

    Add new task, add some new titles

--- Information/Default/Homework.py
+++ Information/Default/Homework.py
@@ -1,6 +1,6 @@
 import Modules.Imports as Imports
 
-test_format = "\t\\drawpage{{{control_event} №{event_number}}}{{{group}}}{{{student}}}{{\n{excercises}\n\t}}\n"
+test_format = "\t\\drawpage{{{control_event} №{event_number}\\\\по дисциплине <<Аналитическая геометрия>>}}{{{group}}}{{{student}}}{{\n{excercises}\n\t}}\n"
 excercise_format = "\t\t\\item {title}\n{tasks}"
 tasks_format = "\t\t\t{task}" 
 
