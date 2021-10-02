f81ec16c486eeac28279ad2c726faa015d2923ba
AriosJentu <darthpoezd@gmail.com>
2021-10-02 16:27:46 +1000

    Add homework 2

--- Information/AssignmentsInformation.py
+++ Information/AssignmentsInformation.py
@@ -3,6 +3,8 @@ AssignmentsInformation = Assignments.AssignmentsInformationClass()
 
 #Imports 
 from .Homework import Homework1
+from .Homework import Homework2
 
 #Append assignments into Assignments Information List
 AssignmentsInformation.append(Homework1.Homework1, "h")
+AssignmentsInformation.append(Homework2.Homework2, "h")
