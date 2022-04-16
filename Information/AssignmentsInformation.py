ebf659ed6b2bfcf8809eedda33a3595055b71e97
Arios Jentu <darthpoezd@gmail.com>
2022-04-16 16:04:21 +1000

    Add comission tasks

--- Information/AssignmentsInformation.py
+++ Information/AssignmentsInformation.py
@@ -6,6 +6,7 @@ from .Homework import Homework1
 from .Homework import Homework2
 from .Homework import Homework3
 from .Exam import Exam
+from .Comission import Comission
 
 #Append assignments into Assignments Information List
 AssignmentsInformation.append(Homework1.Homework1, "h")
@@ -15,3 +16,6 @@ AssignmentsInformation.set_description("h", "Homework tasks for analytic geometr
 
 AssignmentsInformation.append(Exam.Exam, "e")
 AssignmentsInformation.set_description("e", "Exam tasks of analytic geometry course")
+
+AssignmentsInformation.append(Comission.Comission, "c")
+AssignmentsInformation.set_description("c", "Comission tasks")
---
+++ Information/Comission/Comission.py
@@ -0,0 +1,72 @@
+import random
+
+from ..Default import Exam
+import Modules.Imports as Imports
+
+from ..Scripts import Lines
+from ..Scripts import TextReplacer
+
+control_event = "Экзаменационный вариант"
+event_number = 1
+
+prefix = "Tasks/Comission/"
+
+def tasks_updater(text):
+	random.seed()
+
+	line = Lines.Line(3)
+	plane = Lines.HyperPlane(3)
+
+	line.generate_random_line_points()
+	text = text.replace("#LINE#", line.to_random_form())
+
+	plane.generate_random_plane_points()
+	text = text.replace("#PLANE1#", plane.to_random_form())
+
+	plane.generate_random_plane_points()
+	text = text.replace("#PLANE2#", plane.to_random_form())
+
+	for i in range(4):
+		pt = f"#POINT{i+1}#"
+		text = text.replace(pt, Lines.Point.generate_random_point(3).to_str(
+			Lines.generate_point_name()
+		))
+
+	text = TextReplacer.text_replacer(text)
+
+	return text
+
+tasks1 = Imports.Tasks.SpecificTasks()
+task1 = Imports.Tasks.SpecificTaskInfo(prefix+f"Task1.tex")
+task1.set_updater_function(tasks_updater)
+tasks1.append(task1)
+
+tasks2 = Imports.Tasks.SpecificTasks()
+task2 = Imports.Tasks.SpecificTaskInfo(prefix+f"Task2.tex")
+task2.set_updater_function(tasks_updater)
+tasks2.append(task2)
+
+tasks3 = Imports.Tasks.SpecificTasks()
+task3 = Imports.Tasks.SpecificTaskInfo(prefix+f"Task3.tex")
+task3.set_updater_function(tasks_updater)
+tasks3.append(task3)
+
+tasks = [tasks1, tasks2, tasks3]
+
+tasksinfo = []
+for task in tasks:
+	taskinfo = [Imports.Tasks.TasksInformation(task)]
+	tasksinfo.append(taskinfo)
+
+exercises = []
+for taskinfo in tasksinfo:
+	exercise = Imports.Excercises.Excercise(taskinfo)
+	exercises.append(exercise)
+
+class Comission(Imports.Assignments.Assignment):
+
+	layout = Imports.Documents.DocumentLayout(Exam.LayoutLocation, Exam.PageStyle)
+	test = Imports.Tests.Test(exercises)
+	document_entries = Imports.Entries.DocumentEntries(control_event=control_event, event_number=event_number)
+	prefix = "ageom_comission"
+	number = event_number
